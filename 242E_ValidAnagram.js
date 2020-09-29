5087e210b1b942d0a0ee4fa7da59bb2711a0fd2e
Zilong G <45323712+code269@users.noreply.github.com>
2020-09-28 22:22:14 -0400

    Create 242E_ValidAnagram.js

---
+++ 242E_ValidAnagram.js
@@ -0,0 +1,37 @@
+var isAnagram = function(s, t) {
+    let countFirst = {};
+    let countSecond = {};
+    
+    //If BOTH empty, anagram is considered true
+    if (s === "" && t === "") {
+        return true;
+    }
+    
+    //If different amount of letters, not anagram
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    //Adding keys to object & keeping count of occurrence
+    for (const letter of s) {
+        countFirst[letter] = (countFirst[letter] || 0) + 1;
+    }
+    
+    for (const letter of t) {
+        countSecond[letter] = (countSecond[letter] || 0) + 1;
+    }
+    
+    //Comparing objects
+    for (const value in countFirst) {
+        //If keys matchup
+        if (!(value in countSecond)) {
+            return false;
+        }
+        
+        //If values (frequency in this case) of keys matchup
+        if (countFirst[value] !== countSecond[value]) {
+            return false;
+        }
+    }
+    return true;
+};
