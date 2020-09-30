e82e92e69ef18c77f7098dbdcb6ed4b6e5f519e1
Zilong G <45323712+code269@users.noreply.github.com>
2020-09-29 23:28:31 -0400

    Create 242E_ValidAnagram2.js
    Slightly better solution using less memory

---
+++ 242E_ValidAnagram2.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+ 
+//Test cases: 
+//"","" = true
+//"cat", "rat" = false
+//"ca", "rat" = false
+//"anagram", "nagaram" = true
+
+var isAnagram = function(s, t) {
+    let lookup = {};
+    
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    //Fill in object
+    for (let i=0; i<s.length; i++) {
+        let letter = s[i];
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1; //If that key exists, inc 1. Else, init @ 1
+    }
+    
+    //Checks hashtable to see if every key exists and can be subtracted
+    for (let i=0; i<t.length; i++) {
+        let letter = t[i];
+        
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+    return true;
+};
+
+//O(n) S&T - Slightly better than previous solution, uses less space
+//Frequency Count approach
