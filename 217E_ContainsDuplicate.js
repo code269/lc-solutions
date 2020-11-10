e5ff28ac4aeada952e1097671317fa7aaf438729
Zilong Guan <45323712+code269@users.noreply.github.com>
2020-11-10 02:30:07 -0500

    Update 217E_ContainsDuplicate.js
    Add optimized solution

--- 217E_ContainsDuplicate.js
+++ 217E_ContainsDuplicate.js
@@ -3,10 +3,10 @@
  * @return {boolean}
  */
  
+//Naive linear search solution
 //O(n^2) T | O(1) S
  
 var containsDuplicate = function(nums) {
-    //Naive solution
     for (let i=0; i<nums.length-1; i++) {
         for (let j=i+1; j<nums.length; j++) {
             if (nums[i] === nums[j]) return true;
@@ -14,3 +14,22 @@ var containsDuplicate = function(nums) {
     }
     return false;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+//Frequency counter approach w/ Hashtable
+//O(n) T | O(n) S
+
+var containsDuplicate = function(nums) {
+    let duplicate = {};
+    
+    for (let i=0; i<nums.length; i++) {
+        if (!(nums[i] in duplicate)) duplicate[nums[i]] = true;
+        else return true;
+    }
+    return false;
+};
+
